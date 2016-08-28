2ab150d9042bc110f62bb82b8330ce4ace469735
goodmind <andwebar@gmail.com>
2016-08-28 14:26:02 +0500

    fix exported tape, expose nock type
    Now tape methods is typed (skip, createStream etc)
    modified_tape.d.ts is needed because we can't extend external types with nock

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,7 @@
-import nock = require('nock')
 import tape = require('tape')
+import tapeNock = require('./modified_tape')
 
 export = tapeNockFactory
-
-declare function tapeNock (name: string, cb: tape.TestCase): void
-declare function tapeNock (name: string, opts: tape.TestOptions, cb: tape.TestCase): void
-declare function tapeNock (cb: tape.TestCase): void
-declare function tapeNock (opts: tape.TestOptions, cb: tape.TestCase): void
-declare namespace tapeNock {
-  export let nock: typeof nock & {
-    cleanAll: typeof nock.cleanAll
-    disableNetConnect: typeof nock.disableNetConnect
-    enableNetConnect: typeof nock.enableNetConnect
-    recorder: nock.Recorder
-  }
-}
 
 declare function tapeNockFactory (tapeTest: typeof tape, nockOpts: { fixtures: string, mode?: string }): typeof tapeNock
 declare namespace tapeNockFactory {}
